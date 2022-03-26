2a1dbbead52a5eabbdd9fdff12bcdfc3be544325
Bogeshwarara0 <bogeshwararao12042004@gamil.com>
2022-03-26 14:33:49 +0530

    commit

--- Linear-data-structure/array.java
+++ Linear-data-structure/array.java
@@ -2,20 +2,21 @@ import java.util.Scanner;
 
 //proggram to print the hello world in java
 
-// public class  array{
-//     public static void main(String []args) {
-//        System.out.println("Hello World"); // prints Hello World
-//     }
-//  }
+public class  array{
+    public static void main(String []args) {
+       System.out.println("Hello World"); // prints Hello World
+    }
+ }
 
 //proggram to print the looping 
-// public class array{
-//    public static void main(String [] args){
-//            for(int i=0; i<=9 ; i++){
-//             System.out.println(i);
-//            }
-//    }
-// }
+
+public class array{
+   public static void main(String [] args){
+           for(int i=0; i<=9 ; i++){
+            System.out.println(i);
+           }
+   }
+}
 
 //comming to array we have the 5 operation:
 // 1)insertion 
