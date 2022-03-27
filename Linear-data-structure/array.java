eda1ea2121d5d49e96e0efa4c760abbf08a55301
bogeshwararao <91318239+Bogeshwararao@users.noreply.github.com>
2022-03-27 09:09:41 +0530

    Update array.java

--- Linear-data-structure/array.java
+++ Linear-data-structure/array.java
@@ -2,21 +2,21 @@ import java.util.Scanner;
 
 //proggram to print the hello world in java
 
-// public class  array{
-//     public static void main(String []args) {
-//        System.out.println("Hello World"); // prints Hello World
-//     }
-//  }
+public class  array{
+    public static void main(String []args) {
+       System.out.println("Hello World"); // prints Hello World
+    }
+ }
 
 // //proggram to print the looping 
 
-// public class array{
-//    public static void main(String [] args){
-//            for(int i=0; i<=9 ; i++){
-//             System.out.println(i);
-//            }
-//    }
-// }
+public class array{
+   public static void main(String [] args){
+           for(int i=0; i<=9 ; i++){
+            System.out.println(i);
+           }
+   }
+}
 
 //comming to array we have the 5 operation:
 // 1)insertion 
@@ -26,27 +26,27 @@ import java.util.Scanner;
 // 5)Traverse
 
 //insertion an element in an array :
-// public class array{
-//    public static void main(String [] args) {
-//          int size,in_element;
-//          Scanner sc= new Scanner(System.in);
-//          System.out.println("Enter the size of an array");
-//          size = sc.nextInt();
-//          int arr[]= new int[size+1];
-//          for(int i=0;i<size;i++)
-//          {
-//             arr[i]=sc.nextInt();
-//          }
-//          System.out.print("Enter the element which you want to insert:");
-//          in_element=sc.nextInt();
-//          arr[size] = in_element;
-//         System.out.print("After inserting : ");
-//         for(int i = 0; i <size; i++)
-//         {
-//             System.out.print(arr[i]+",");
-//         }   
-//    }
-// }
+public class array{
+   public static void main(String [] args) {
+         int size,in_element;
+         Scanner sc= new Scanner(System.in);
+         System.out.println("Enter the size of an array");
+         size = sc.nextInt();
+         int arr[]= new int[size+1];
+         for(int i=0;i<size;i++)
+         {
+            arr[i]=sc.nextInt();
+         }
+         System.out.print("Enter the element which you want to insert:");
+         in_element=sc.nextInt();
+         arr[size] = in_element;
+        System.out.print("After inserting : ");
+        for(int i = 0; i <size; i++)
+        {
+            System.out.print(arr[i]+",");
+        }   
+   }
+}
 
 //deletion of an element in an array:
 public class array{
