fa49c541dccf007e0d07e827c842ef380a5a9001
Bogeshwarara0 <bogeshwararao12042004@gamil.com>
2022-03-30 17:26:06 +0530

    commit

---
+++ Linear-data-structure/linked.py
@@ -0,0 +1,27 @@
+class Node:
+   def __init__(self, dataval):
+      self.dataval = dataval
+      self.nextval = None
+
+class LinkedList:
+   def __init__(self):
+      self.headval = None
+
+   def listprint(self):
+      printval = self.headval
+      while printval is not None:
+         print (printval.dataval)
+         printval = printval.nextval
+
+list = LinkedList()
+list.headval = Node(1)
+e2 = Node(2)
+e3 = Node(3)
+
+# Link first Node to second node
+list.headval.nextval = e2
+
+# Link second Node to third node
+e2.nextval = e3
+
+list.listprint()
