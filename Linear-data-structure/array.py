d8417475f8e92828a399013a771cbf8ce83786c4
Bogeshwarara0 <bogeshwararao12042004@gamil.com>
2022-03-31 16:53:34 +0530

    commit

---
+++ Linear-data-structure/array.py
@@ -0,0 +1,12 @@
+#proggram for insertion sort
+list= [1,2,3,4,9,5]
+for i in range(1,len(list)):
+    j=i-1
+    while j>=0 and list[j]>list[j+1]:
+        t=list[j]
+        list[j]=list[j+1]
+        list[j+1]=t
+        j=j-1
+
+
+print(list)
