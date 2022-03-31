ff1156a47d60247246f92e273b1d276646ff2c27
Bogeshwarara0 <bogeshwararao12042004@gamil.com>
2022-03-31 17:14:46 +0530

    commit

--- Linear-data-structure/array.py
+++ Linear-data-structure/array.py
@@ -1,21 +1,14 @@
 #proggram for insertion sort
-list= [1,2,3,4,9,5]
-for i in range(1,len(list)):
-    j=i-1
-    while j>=0 and list[j]>list[j+1]:
-        t=list[j]
-        list[j]=list[j+1]
-        list[j+1]=t
-        j=j-1
-print(list)
+# list= [1,2,3,4,9,5]
+# for i in range(1,len(list)):
+#     j=i-1
+#     while j>=0 and list[j]>list[j+1]:
+#         t=list[j]
+#         list[j]=list[j+1]
+#         list[j+1]=t
+#         j=j-1
+# print(list)
 
-#proggram for insertion sort
-list= [1,2,3,4,9,5]
-for i in range(1,len(list)):
-    j=i-1
-    while j>=0 and list[j]>list[j+1]:
-        t=list[j]
-        list[j]=list[j+1]
-        list[j+1]=t
-        j=j-1
-print(list)
+
+#proggram to insert an elemnet
+list=[1,2,3,4,5]
