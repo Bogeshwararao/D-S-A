a196f9a32ad6f40a1a44c0e3ea7aa3d842bf0ce4
Bogeshwarara0 <bogeshwararao12042004@gamil.com>
2022-05-01 16:16:09 +0530

    commit

---
+++ Linear-data-structure/sort.java
@@ -0,0 +1,10 @@
+public class  sort{
+    public static void main(String []args) {
+      
+    }
+    private static void quicksort(int a[],int lowindex,int highindex ) {
+   
+                   
+     
+    }
+}
