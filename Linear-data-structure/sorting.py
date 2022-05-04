3845f04c10be1c65d49613ff3d0c017c4988afaf
bogeshwararao <91318239+Bogeshwararao@users.noreply.github.com>
2022-05-04 23:04:43 +0530

    Update sorting.py

--- Linear-data-structure/sorting.py
+++ Linear-data-structure/sorting.py
@@ -22,5 +22,57 @@ def qs(a,f,n):
 a=[11,3,6,12,15]
 qs(a,0,len(a)-1)
 print(a)
-   
+ #merge sort:
+def mergeSort(arr):
 
+    if len(arr) > 1:
+
+        a = len(arr)//2
+
+        l = arr[:a]
+
+        r = arr[a:]
+
+        # Sort the two halves
+
+        mergeSort(l)
+
+        mergeSort(r) 
+
+        b = c = d = 0
+
+        while b < len(l) and c < len(r):
+
+            if l[b] < r[c]:
+
+                arr[d] = l[b]
+
+                b += 1
+
+            else:
+
+                arr[d] = r[c]
+
+                c += 1
+
+            d += 1
+
+        while b < len(l):
+
+            arr[d] = l[b]
+
+            b += 1
+
+            d += 1
+
+        while c < len(r):
+
+            arr[d] = r[c]
+
+            c += 1
+
+            d += 1
+
+arr=[4,3,7,6,5,3,1,2]
+mergeSort(arr)
+print(arr)
